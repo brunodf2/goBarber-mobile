1d0a95b02fb44dc3cd13796e37ccc4471b513aa5
brunodf2 <bruno.araujodf2@gmail.com>
2019-08-14 01:00:46 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createBottomTabNavigator,
+  createStackNavigator,
+} from 'react-navigation';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
+
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#FFF',
+                    headerLeftContainerStyle: {
+                      marginLeft: 15,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarLabel: 'Agendar',
+                tabBarVisible: false,
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.7)"
+                  />
+                ),
+              },
+            },
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              activeBackgroundColor: '#8d41a8',
+              inactiveBackgroundColor: '#8d41a8',
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
